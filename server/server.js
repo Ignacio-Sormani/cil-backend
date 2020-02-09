bcbf0ef186998ea5eb0d168a22017c101215e0ee
Ignacio Sormani <ignaciosormani@gmail.com>
2020-02-09 03:13:10 -0300

    CIL-4: connection to atlas mongodb

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const router = require('./routes');
 
 const app = express();
@@ -7,6 +8,14 @@ const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+mongoose.connect(
+  'mongodb+srv://CIL-ADMIN:cil2019@cil-cluster-ldtk8.gcp.mongodb.net/database?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
  
 app.use(router);
 
