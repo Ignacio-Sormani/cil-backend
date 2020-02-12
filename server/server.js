566fb47d1ab1ec697d4145e4180cc2c688a3ee87
Ignacio Sormani <ignaciosormani@gmail.com>
2020-02-11 22:04:47 -0300

    CIL-6: add cors handlers

--- server/server.js
+++ server/server.js
@@ -9,6 +9,19 @@ const port = 4000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', 'POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 mongoose.set('useCreateIndex', true);
 mongoose.connect(
   'mongodb+srv://CIL-ADMIN:cil2019@cil-cluster-ldtk8.gcp.mongodb.net/database?retryWrites=true&w=majority',
