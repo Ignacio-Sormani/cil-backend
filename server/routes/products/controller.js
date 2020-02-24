356ad725903eaf724c57f5e1765fbd776811bb29
Claudia V. Lima <limaclaudia@hotmail.com>
2020-02-24 18:27:42 -0300

    CIL-25: create get active products endpoint

--- server/routes/products/controller.js
+++ server/routes/products/controller.js
@@ -1,37 +1,36 @@
-const Category = require('../../models/category');
-const Product = require('../../models/product');
+const Category = require("../../models/category");
+const Product = require("../../models/product");
 
 const getProducts = (req, res) => {
   Product.find()
-    .select('_id name description category price stock isActive')
-    .populate('category', 'name description')
+    .select("_id name description category price stock isActive")
+    .populate("category", "name description")
     .exec()
     .then(response => {
       if (response.length > 0) {
         res.status(200).json({
-          message: 'Products were retrieved!',
+          message: "Products were retrieved!",
           data: response
         });
-      }
-      else {
+      } else {
         res.status(404).json({
-          error: 'No products found!'
+          error: "No products found!"
         });
       }
     })
     .catch(() => {
       res.status(500).json({
-        error: 'Products could not be retrieved!'
+        error: "Products could not be retrieved!"
       });
     });
 };
 
 const postProduct = (req, res) => {
   Category.findById(req.body.category)
-    .then((category) => {
+    .then(category => {
       if (!category) {
         res.status(404).json({
-          error: 'Category not found'
+          error: "Category not found"
         });
       }
       const product = new Product(req.body);
@@ -39,7 +38,7 @@ const postProduct = (req, res) => {
     })
     .then(response => {
       res.status(201).json({
-        message: 'Product was created!',
+        message: "Product was created!",
         data: {
           _id: response.id,
           name: response.name,
@@ -53,33 +52,32 @@ const postProduct = (req, res) => {
     })
     .catch(() => {
       res.status(500).json({
-        error: 'Product could not be created!'
+        error: "Product could not be created!"
       });
     });
 };
 
 const modifyProduct = (req, res) => {
   Product.findByIdAndUpdate(req.params.productId, req.body, { new: true })
-    .populate('category', 'name description')
+    .populate("category", "name description")
     .exec()
-    .then((response) => {
-      if (response) {        
+    .then(response => {
+      if (response) {
         res.status(201).json({
-          message: 'Product was modified!',
+          message: "Product was modified!",
           data: response
-        });         
-      }
-      else {
+        });
+      } else {
         res.status(404).json({
-          error: 'Product not found!'
+          error: "Product not found!"
         });
       }
     })
     .catch(() => {
-    res.status(500).json({
-      error: 'Product could not be modified!'
+      res.status(500).json({
+        error: "Product could not be modified!"
+      });
     });
-  });
 };
 
 const deleteProduct = (req, res) => {
@@ -88,21 +86,50 @@ const deleteProduct = (req, res) => {
     .then(response => {
       if (response) {
         res.status(201).json({
-          message: 'Product was deleted!',
+          message: "Product was deleted!",
           data: response
         });
-      }
-      else {
+      } else {
         res.status(404).json({
-          error: 'Product not found!'
+          error: "Product not found!"
         });
       }
     })
     .catch(() => {
       res.status(500).json({
-        error: 'Product could not be deleted!'
+        error: "Product could not be deleted!"
       });
     });
 };
 
-module.exports = { postProduct, getProducts, modifyProduct, deleteProduct };
+const getActiveProducts = (req, res) => {
+  Product.find({ isActive: true })
+    .select("_id name description category price stock isActive")
+    .populate("category", "name description")
+    .exec()
+    .then(response => {
+      if (response.length > 0) {
+        res.status(200).json({
+          message: "Products were retrieved!",
+          data: response
+        });
+      } else {
+        res.status(404).json({
+          error: "No products found!"
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "Products could not be retrieved!"
+      });
+    });
+};
+
+module.exports = {
+  postProduct,
+  getProducts,
+  modifyProduct,
+  deleteProduct,
+  getActiveProducts
+};
--- server/routes/products/index.js
+++ server/routes/products/index.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { postProduct, getProducts, modifyProduct, deleteProduct } = require('./controller');
-const checkToken = require('../../middleware/check-token');
+const {
+  postProduct,
+  getProducts,
+  modifyProduct,
+  deleteProduct,
+  getActiveProducts
+} = require("./controller");
+const checkToken = require("../../middleware/check-token");
 
 router
-  .route('/')
+  .route("/")
   .post(checkToken, postProduct)
   .get(checkToken, getProducts);
 
 router
-  .route('/:productId')
+  .route("/:productId")
   .patch(checkToken, modifyProduct)
   .delete(checkToken, deleteProduct);
+
+router.route("/active").get(getActiveProducts);
 
 module.exports = router;
