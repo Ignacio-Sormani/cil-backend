d91aae2b076a502ddb69ccc2209daa817867c5a0
Ignacio Sormani <ignaciosormani@gmail.com>
2020-06-17 22:58:05 -0300

    CIL-63: remove heroku configs

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.connect(
-  'mongodb+srv://CIL-ADMIN:cil2019@cil-cluster-ldtk8.gcp.mongodb.net/database?retryWrites=true&w=majority',
+  'mongodb+srv://admin:DI7m9TSMaM6SrS3X@sap-project-ldtk8.mongodb.net/database?retryWrites=true&w=majority',
   {
     useUnifiedTopology: true,
     useNewUrlParser: true
